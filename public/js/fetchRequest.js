3615ae61c8b206571813dd9ef15566b27f3159bc
Faheem Abbas <faheem.abbas@skyelectric.com>
2020-02-25 17:40:49 +0500

    setup for heroku

--- public/js/fetchRequest.js
+++ public/js/fetchRequest.js
@@ -18,7 +18,7 @@ weatherForm.addEventListener('submit', (e) => {
     getForecast(location)
 })
 function getForecast(location) {
-    fetch('http://localhost:3000/weather?address=' + location).then((response) => {
+    fetch('/weather?address=' + location).then((response) => {
 
         response.json().then((data) => {
 
